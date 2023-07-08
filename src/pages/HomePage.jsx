1d4a29e7017851b262038dcd7e45e75db2bc5a6f
Harsh Mohite <harsh.mohite009@gmail.com>
2023-07-08 12:41:30 +0530

    initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,10 @@
-import { Button, Flex, Heading, useDisclosure } from "@chakra-ui/react";
 import React from "react";
-import Filter from "../components/Filter";
-import { useAppContext } from "../context/AppContextProvider";
-import RecipeList from "../components/RecipeList";
-import RecipeModal from "../components/RecipeModal";
+import { Flex, Heading } from "@chakra-ui/react";
 
 const HomePage = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const {
-    state: { recipes },
-  } = useAppContext();
-
   return (
-    <Flex as={"main"} flexDir="column" gap={4} p={4}>
-      <Filter />
-      <Button w="fit-content" onClick={onOpen}>
-        Create New Recipe
-      </Button>
-      <RecipeModal isOpen={isOpen} onClose={onClose} />
-      <Heading>All Recipies: </Heading>
-      <RecipeList recipes={recipes} />
+    <Flex as={"main"} flexDir="column" p={4}>
+      <Heading>MCR-6</Heading>
     </Flex>
   );
 };
